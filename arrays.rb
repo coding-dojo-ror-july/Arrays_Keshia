b94ad00329c3f4c579eaeb6084dcd6eeedf3054e
Keshia <kee19.kg@gmail.com>
2017-07-03 21:39:42 +0000

    initial commit

---
+++ arrays.rb
@@ -0,0 +1,24 @@
+a = ["Keshia", "Kee", "Kesh","Key","Ke-Ke"]
+
+puts a.at(2)
+
+
+puts a.fetch(1)
+
+puts a.delete(0)
+
+puts a.reverse
+
+puts a.length
+
+puts a.sort
+
+puts a.slice(0)
+
+puts a.shuffle
+
+puts a.join
+
+puts a.insert(1, "keyz")
+
+puts a.values_at(0, 1, 4)
